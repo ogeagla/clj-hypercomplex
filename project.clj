79299f6b44f45e3b54b4808aabfccf27b11145c4
Octavian Geagla <ogeagla@gmail.com>
2018-09-18 09:09:25 -0600

    [style] linting lein plugins

--- project.clj
+++ project.clj
@@ -3,6 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins      [[lein-ancient "0.6.14"]
+                 [jonase/eastwood "0.2.5"]
+                 [lein-kibit "0.1.5"]
+                 [lein-bikeshed "0.5.0"]]
   :dependencies [[org.clojure/clojure "1.10.0-alpha7"]
                  [org.apache.commons/commons-math3 "3.6.1"]]
   :main ^:skip-aot og.cayley-dickson.core
