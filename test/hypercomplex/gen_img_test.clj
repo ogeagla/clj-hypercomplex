07bc1c120c3b46ec366d406080425a6cbec8c5b3
Octavian Geagla <ogeagla@gmail.com>
2018-12-06 16:08:30 -0700

    [gen-test] zoom on fractal

--- test/hypercomplex/gen_img_test.clj
+++ test/hypercomplex/gen_img_test.clj
@@ -57,7 +57,7 @@
 (defn pgen-spec [generator quant]
   (pmap
     (fn [i]
-      (when (= 0 (mod i 50000))
+      (when (= 0 (mod i (int (/ quant 10))))
         (println "Generating intensity " i " / " quant " , "
                  (int (* 100 (/ i quant))) "% complete"))
       (gen/generate generator))
@@ -68,60 +68,60 @@
     (println "Generating frac")
     (let [c* (atom 1)]
       (doseq [test-size [
-                         1000000
+                         400000
                          ;400000
                          ;800000
                          ;;this one takes ~80 seconds
                          ;1600000
-                         ;3200000
-                         ;6400000
                          ;;this one might take 10 mins:
                          ;12800000
-                         ;25600000
-                         ;51200000
                          ;;this last one might take 80 minutes or so:
                          ;102400000
                          ]]
-
-        (doseq [[xrange yrange] [
-                                 [[-0.3 0.2] [-1.0 -0.5]]
-                                 [[-0.28 0.18] [-0.98 -0.52]]
-                                 [[-0.26 0.16] [-0.96 -0.54]]
-                                 [[-0.24 0.14] [-0.94 -0.56]]
-                                 [[-0.22 0.12] [-0.92 -0.58]]
-                                 [[-0.21 0.11] [-0.91 -0.59]]
-                                 [[-0.20 0.10] [-0.90 -0.60]]
-                                 [[-0.19 0.09] [-0.89 -0.61]]
-                                 [[-0.185 0.085] [-0.885 -0.615]]
-                                 [[-0.18 0.08] [-0.88 -0.62]]
-                                 [[-0.175 0.075] [-0.875 -0.625]]
-                                 [[-0.17 0.07] [-0.87 -0.63]]
-
-                                 ]]
-          (reset! gt/X-RANGE* xrange)
-          (reset! gt/Y-RANGE* yrange)
-          (let [
-                ;test-size 200000
-                w     2000
-                h     1200
-                image (BufferedImage.
-                        w
-                        h
-                        BufferedImage/TYPE_INT_ARGB)
-                its   (pgen-spec
-                        (s/gen
-                          ::gt/interesting-intensity-plain)
-                        test-size)
-                imgf  (str "gen-img-test/gen-img-test-"
-                           (System/currentTimeMillis) "-"
-                           test-size "-" xrange "-" yrange ".png")]
-            (swap! c* inc)
-            (println "Drawing size: "
-                     test-size (count its) (count (set its)))
-            (draw-intensities-w-raster its image w h)
-            (println "Show!")
-            ;(imgz/show image)
-            (imgz/save image imgf)
-            (println "Wrote: " imgf)))))))
+        (let [x-center  0.005
+              x-width0  0.1
+              x-widthf  0.0001
+              y-center  -0.75
+              y-height0 0.1
+              y-heightf 0.0001
+              tx        (- x-width0 x-widthf)
+              ty        (- y-height0 y-heightf)
+              views     10
+              domans    (map
+                          (fn [i]
+                            (let [pct (/ i views)
+                                  dx  (+ (/ x-widthf 2) (* pct (/ tx 2)))
+                                  dy  (+ (/ y-heightf 2) (* pct (/ ty 2)))]
+                              [[(- x-center dx)
+                                (+ x-center dx)]
+                               [(- y-center dy)
+                                (+ y-center dy)]]))
+                          (range views 1 -1))]
+          (doseq [[xrange yrange] domans]
+            (println "Domain: " xrange yrange)
+            (reset! gt/X-RANGE* xrange)
+            (reset! gt/Y-RANGE* yrange)
+            (let [w     2000
+                  h     1200
+                  image (BufferedImage.
+                          w
+                          h
+                          BufferedImage/TYPE_INT_ARGB)
+                  its   (pgen-spec
+                          (s/gen
+                            ::gt/interesting-intensity-plain)
+                          test-size)
+                  imgf  (str "gen-img-test/gen-img-test-"
+                             (System/currentTimeMillis) "-" test-size
+                             "-x-" (clojure.string/join "-" xrange)
+                             "-y-" (clojure.string/join "-" yrange) ".png")]
+              (swap! c* inc)
+              (println "Drawing size: "
+                       test-size (count its) (count (set its)))
+              (draw-intensities-w-raster its image w h)
+              (println "Show!")
+              ;(imgz/show image)
+              (imgz/save image imgf)
+              (println "Wrote: " imgf))))))))
 
 (run-tests 'hypercomplex.gen-img-test)
