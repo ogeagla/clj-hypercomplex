5432327b2ece301cf07218d8a661d9e35cad6bad
Octavian Geagla <ogeagla@gmail.com>
2018-12-06 23:10:00 -0700

    [gen-test]

--- test/hypercomplex/gen_img_test.clj
+++ test/hypercomplex/gen_img_test.clj
@@ -68,11 +68,36 @@
       (gen/generate generator))
     (range quant)))
 
+(defn width->iters [w]
+  (cond
+    (> w 0.5) 32
+    (> w 0.4) 36
+    (> w 0.3) 40
+    (> w 0.1) 45
+    (> w 0.05) 50
+    (> w 0.04) 55
+    (> w 0.03) 60
+    (> w 0.01) 85
+    (> w 0.008) 90
+    (> w 0.006) 95
+    (> w 0.005) 100
+    (> w 0.003) 105
+    (> w 0.002) 110
+    (> w 0.001) 125
+    (> w 0.00075) 150
+    (> w 0.0005) 160
+    (> w 0.0001) 180
+    (> w 0.00005) 210
+    (> w 0.00001) 230
+    (> w 0.00005) 250
+    (> w 0.000001) 270
+    :else 300))
+
 (deftest gen-img-test
   (testing "Generates fractal image"
     (println "Generating frac")
     (doseq [test-size [
-                       50000
+                       20000
                        ;400000
                        ;800000
                        ;;this one takes ~80 seconds
@@ -82,55 +107,39 @@
                        ;;this last one might take 80 minutes or so:
                        ;102400000
                        ]]
-      (let [x-center     -0.01
-            x-width0     0.05
-            x-widthf     0.00001
-            y-center     -0.8
-            y-height0    0.05
-            y-heightf    0.00001
-            tx           (- x-width0 x-widthf)
-            ty           (- y-height0 y-heightf)
-            views        100
-            ;TODO upside down log:
-            width->iters (fn [w]
-                           (cond
-                             (> w 0.5) 32
-                             (> w 0.4) 36
-                             (> w 0.3) 40
-                             (> w 0.1) 45
-                             (> w 0.05) 50
-                             (> w 0.04) 55
-                             (> w 0.03) 60
-                             (> w 0.01) 70
-                             (> w 0.005) 85
-                             (> w 0.001) 100
-                             (> w 0.0005) 120
-                             (> w 0.0001) 150
-                             :else 200))
-            domans       (map
-                           (fn [i]
-                             (let [pct (/ i views)
-                                   pct (cond
-                                         ;for fast initial zoom
-                                         (> pct 0.75) (- 1.0 (* 2 (- 1.0 pct)))
-                                         (> pct 0.5) (- pct 0.25)
-                                         :else (/ pct 2.0))
-                                   dx  (+ (/ x-widthf 2) (* pct (/ tx 2)))
-                                   dy  (+ (/ y-heightf 2) (* pct (/ ty 2)))]
-
-                               [[(- x-center dx)
-                                 (+ x-center dx)]
-                                [(- y-center dy)
-                                 (+ y-center dy)]
-                                (width->iters (* 2 dx))]))
-                           (range views 0 -1))]
+      (let [x-center  -0.01
+            x-width0  0.7
+            x-widthf  0.000001
+            y-center  -0.8
+            y-height0 0.7
+            y-heightf 0.000001
+            tx        (- x-width0 x-widthf)
+            ty        (- y-height0 y-heightf)
+            views     50
+            domans    (map
+                        (fn [i]
+                          (let [pctr (/ i views)
+                                pct (cond
+                                      ;for fast initial zoom
+                                      (>= pctr 0.95) (- 1.0 (* 10 (- 1.0 pctr)))
+                                      (>= pctr 0.5) (- pctr 0.45)
+                                      :else (/ pctr 10.0))
+                                dx  (+ (/ x-widthf 2) (* pct (/ tx 2)))
+                                dy  (+ (/ y-heightf 2) (* pct (/ ty 2)))]
+                            (println "---- Percent mapping: " pctr "->" pct)
+                            [[(- x-center dx)
+                              (+ x-center dx)]
+                             [(- y-center dy)
+                              (+ y-center dy)]
+                             (width->iters (* 2 dx))]))
+                        (range views 0 -1))]
         (doseq [[xrange yrange iters] domans]
           (println "Domain, iters : " xrange yrange iters)
           (reset! gt/X-RANGE* xrange)
           (reset! gt/Y-RANGE* yrange)
           (reset! gt/MAX-CONV-ITERS* iters)
-          (let [w     300
-                h     300
+          (let [w     80
+                h     80
                 image (BufferedImage.
                         w
                         h
@@ -148,7 +157,7 @@
             (draw-intensities-w-raster its image w h)
             (println "Show!")
             ;(imgz/show image)
-            (imgz/save image imgf)
+            (imgz/save (imgz/scale image 4) imgf)
             (println "Wrote: " imgf)))))))
 
 (run-tests 'hypercomplex.gen-img-test)
