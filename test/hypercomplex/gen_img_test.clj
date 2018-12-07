7b4ca9234f0f480c0772a39e46cf9bf3d8bdaaa2
Octavian Geagla <ogeagla@gmail.com>
2018-12-07 10:07:59 -0700

    [gen-test][img] test run config

--- test/hypercomplex/gen_img_test.clj
+++ test/hypercomplex/gen_img_test.clj
@@ -11,16 +11,14 @@
 (set! *warn-on-reflection* true)
 
 (defn- calc-pixel-color-argb
-  "Returns an int array representing an ARGB color
-  derived from iterations, with alpha = 1.0."
   [iterations max-iterations]
-  (if (or (< iterations 10)
-          (= iterations max-iterations))
-    (int-array [1.0 0 0 0])
+  (if (or #_(< iterations 10)
+        (= iterations max-iterations))
+    (int-array [220 230 22 255])
     (let [gray (int (/ (* iterations 255) max-iterations))
-          r    (min (int (+ 90 (/ (* 5 (* gray gray)) 255))) 255)
-          g    (min (int (/ (* 3 (* gray gray)) 255)) 255)
-          b    gray]
+          r    (min (int (+ 20 gray)) 255)
+          g    (min (int (- 255 gray)) 255)
+          b    (min (int (Math/abs (- gray 80))) 255)]
       (int-array [r g b 255]))))
 
 (defn intensity->img-coord
@@ -61,7 +59,7 @@
 (defn pgen-spec [generator quant]
   (pmap
     (fn [i]
-      (when (= 0 (mod i (int (/ quant 10))))
+      (when (= 0 (mod i (int (/ quant 20))))
         (println "Generating intensity " i " / " quant " , "
                  (int (* 100 (/ i quant))) "% complete, t="
                  (System/currentTimeMillis)))
@@ -93,71 +91,83 @@
     (> w 0.000001) 270
     :else 300))
 
+(defn fractal-config [c]
+  (case c
+    :c1 {:x-center  -0.01
+         :y-center  -0.80
+         :type      :edge-spiral-arms
+         :x-width0  0.4
+         :x-widthf  0.004
+         :y-height0 0.4
+         :y-heightf 0.004
+         }))
+
+(defn render-config [mode]
+  (case mode
+    :fast {:test-size    100000
+           :views        50
+           :img-w        300
+           :img-h        300
+           :img-scale-fn #(imgz/scale % 4)}
+    :medium {:test-size    500000
+             :views        10
+             :img-w        400
+             :img-h        400
+             :img-scale-fn #(imgz/scale % 3)}
+    :slow {:test-size    2000000
+           :views        5
+           :img-w        1200
+           :img-h        1200
+           :img-scale-fn identity}
+    :render {:test-size    50000000
+             :views        5
+             :img-w        1200
+             :img-h        1200
+             :img-scale-fn identity})
+  )
+
 (deftest gen-img-test
   (testing "Generates fractal image"
     (println "Generating frac")
-    (doseq [test-size [
-                       20000
-                       ;400000
-                       ;800000
-                       ;;this one takes ~80 seconds
-                       ;1600000
-                       ;;this one might take 10 mins:
-                       ;12800000
-                       ;;this last one might take 80 minutes or so:
-                       ;102400000
-                       ]]
-      (let [x-center  -0.01
-            x-width0  0.7
-            x-widthf  0.000001
-            y-center  -0.8
-            y-height0 0.7
-            y-heightf 0.000001
-            tx        (- x-width0 x-widthf)
-            ty        (- y-height0 y-heightf)
-            views     50
-            domans    (map
-                        (fn [i]
-                          (let [pctr (/ i views)
-                                pct (cond
-                                      ;for fast initial zoom
-                                      (>= pctr 0.95) (- 1.0 (* 10 (- 1.0 pctr)))
-                                      (>= pctr 0.5) (- pctr 0.45)
-                                      :else (/ pctr 10.0))
-                                dx  (+ (/ x-widthf 2) (* pct (/ tx 2)))
-                                dy  (+ (/ y-heightf 2) (* pct (/ ty 2)))]
-                            (println "---- Percent mapping: " pctr "->" pct)
-                            [[(- x-center dx)
-                              (+ x-center dx)]
-                             [(- y-center dy)
-                              (+ y-center dy)]
-                             (width->iters (* 2 dx))]))
-                        (range views 0 -1))]
-        (doseq [[xrange yrange iters] domans]
-          (println "Domain, iters : " xrange yrange iters)
-          (reset! gt/X-RANGE* xrange)
-          (reset! gt/Y-RANGE* yrange)
-          (reset! gt/MAX-CONV-ITERS* iters)
-          (let [w     80
-                h     80
-                image (BufferedImage.
-                        w
-                        h
-                        BufferedImage/TYPE_INT_ARGB)
-                its   (pgen-spec
-                        (s/gen
-                          ::gt/interesting-intensity-plain)
-                        test-size)
-                imgf  (str "gen-img-test/gen-img-test-i" iters "-"
-                           (System/currentTimeMillis) "-" test-size
-                           "-x-" (first xrange) "-" (second xrange)
-                           "-y-" (first yrange) "-" (second yrange) ".png")]
-            (println "Drawing size: "
-                     test-size (count its) (count (set its)))
-            (draw-intensities-w-raster its image w h)
-            (println "Show!")
-            ;(imgz/show image)
-            (imgz/save (imgz/scale image 4) imgf)
-            (println "Wrote: " imgf)))))))
+    (let [{:keys
+           [x-center y-center x-width0
+            x-widthf y-height0 y-heightf]} (fractal-config :c1)
+          {:keys
+           [test-size views img-w
+            img-h img-scale-fn]} (render-config :fast)
+          tx     (- x-width0 x-widthf)
+          ty     (- y-height0 y-heightf)
+          domans (map
+                   (fn [i]
+                     (let [pct (/ i views)
+                           dx  (+ (/ x-widthf 2) (* pct (/ tx 2)))
+                           dy  (+ (/ y-heightf 2) (* pct (/ ty 2)))]
+                       [[(- x-center dx)
+                         (+ x-center dx)]
+                        [(- y-center dy)
+                         (+ y-center dy)]
+                        (width->iters (* 2 dx))]))
+                   (range views 0 -1))]
+      (doseq [[xrange yrange iters] domans]
+        (println "Domain, iters, size : " xrange yrange iters test-size)
+        (reset! gt/X-RANGE* xrange)
+        (reset! gt/Y-RANGE* yrange)
+        (reset! gt/MAX-CONV-ITERS* iters)
+        (let [image (BufferedImage.
+                      img-w
+                      img-h
+                      BufferedImage/TYPE_INT_ARGB)
+              its   (pgen-spec
+                      (s/gen
+                        ::gt/interesting-intensity-plain)
+                      test-size)
+              imgf  (str "gen-img-test/gen-img-test-i" iters "-"
+                         (System/currentTimeMillis) "-" test-size
+                         "-x-" (first xrange) "-" (second xrange)
+                         "-y-" (first yrange) "-" (second yrange) ".png")]
+          (draw-intensities-w-raster its image w h)
+          ;(imgz/show image)
+          (imgz/save (img-scale-fn image) imgf)
+          (println "Wrote: " imgf))))))
 
 (run-tests 'hypercomplex.gen-img-test)
