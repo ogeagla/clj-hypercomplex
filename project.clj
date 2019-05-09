a83930957d7acc49faac93548357c4e266ae709c
Octavian Geagla <ogeagla@gmail.com>
2019-05-09 00:20:03 -0600

    [style] style fixes

--- project.clj
+++ project.clj
@@ -4,11 +4,13 @@
   :signing {:gpg-key "ogeagla@gmail.com"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins      [[lein-ancient "0.6.14"]
-                 [jonase/eastwood "0.2.5"]
-                 [lein-kibit "0.1.5"]
-                 [lein-bikeshed "0.5.0"]]
-  :dependencies [[org.clojure/clojure "1.10.0-beta7"]
+  :plugins      [[lein-ancient "0.6.15"]
+                 [jonase/eastwood "0.3.5"]
+                 [lein-kibit "0.1.6"]
+                 [lein-bikeshed "0.5.2"]
+                 [venantius/yagni "0.1.7"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/test.check "0.10.0-alpha3"]
                  [org.apache.commons/commons-math3 "3.6.1"]]
   :main ^:skip-aot hypercomplex.core
   :target-path "target/%s"
