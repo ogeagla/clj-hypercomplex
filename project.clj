392c08be7882739fa9a14533a0dd334122c47467
Octavian Geagla <ogeagla@gmail.com>
2022-08-07 13:09:34 -0600

    [deps] bump

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :signing {:gpg-key "ogeagla@gmail.com"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins      [[lein-ancient "0.6.15"]
-                 [jonase/eastwood "0.3.5"]
-                 [lein-kibit "0.1.6"]
+  :plugins      [[lein-ancient "0.7.0"]
+                 [jonase/eastwood "1.2.4"]
+                 [lein-kibit "0.1.8"]
                  [lein-bikeshed "0.5.2"]
                  [venantius/yagni "0.1.7"]]
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/test.check "0.10.0-alpha3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/test.check "1.1.1"]
                  [org.apache.commons/commons-math3 "3.6.1"]]
   :main ^:skip-aot hypercomplex.core
   :target-path "target/%s"
